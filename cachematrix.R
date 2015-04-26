a143c3b539c8e8efa8276cd22275caf874a3da40
marcelMerchat <merchat77@gmail.com>
2015-04-25 21:21:48 -0500

    Submit Version-1.

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,40 @@
-## Calculating the inverse of a matrix can be computation intensive. Once 
-## the inverse has been computed, we can eliminate repeating the calculation
-## by storing the inverse in a special matrix object that stores both the 
-## matrix and its inverse. 
+## Filename: casheMatrix.R
+
+## Calculates the inverse of a matrix which can be intensive. Once the inverse
+## has been computed, we avoid repeating the calculation by storing the result
+## in a special matrix object that stores both the original matrix and its
+## inverse. 
 
 ## The makeCacheMatrix function below constructs a generalized matrix object
-## that stores both itself and its inverse. the inverse from the
-## generalized matrix object 
+## that stores both itself and its inverse.  
 
 makeCacheMatrix <- function(x = matrix()) {
 
-## The determinant of the matrix x must be not be equal to zero.
-## Otherwise, the inverse does not exist.
+## The determinant of the matrix x must be unequal to zero, otherwise, the
+## inverse does not exist.
         
         inverseMatrix = NULL
         
-        ## The following function can be accessed by the user to create a
-        ## generalized matrix object that the cacheSolve() function
+        ## The following internal function can be accessed by the user  
+        ## to create a generalized matrix object that the cacheSolve() function  
         ## can access to store and retrieve the matrix inverse once it has 
         ## been calculated.
         
         setMatrix = function(y) {
                 
                 # Assign values to the generalized matrix object
-                # outside this global environment 
+                # outside of this local environment 
                 x <<- y
                 inverseMatrix = NULL                
                 }
         
-        ## The generalized matrix object has three more special 
+        ## The generalized matrix object has four special 
         ## functions that the cacheSolve() function can access:
         
-        ## 1. setMatrix - save the input matrix x in memory
-        ## 2. getMatrix - retrieve the matrix x from memory
-        ## 3. setInverse - save the inverse in memory
-        ## 4. getInverse - retrieve the inverse from memory if the 
+        ## 1. setMatrix()  - save the input matrix x in memory
+        ## 2. getMatrix()  - retrieve the matrix x from memory
+        ## 3. setInverse() - save the inverse in memory
+        ## 4. getInverse() - retrieve the inverse from memory if the 
         ##                 inverse has already been calculated.
         
         getMatrix  = function() {x}
@@ -46,12 +47,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-
-## The following function below computes the inverse of a matrix if it has not
-## already been calculated and stored within the generalized matrix object.
-## If the inverse has already been calculated, this function retrieves the
-## inverse matrix from cache memory using the getInverse function of the
-## generalized matrix object created by the makeCacheMatrix function above.
+## The following function computes the inverse of a matrix if it has not been
+## already been calculated. If the inverse has already been calculated, the
+## inverse matrix is retrieved from cache memory using the getInverse()
+## function of the generalized matrix object created by the makeCacheMatrix
+## function above.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
